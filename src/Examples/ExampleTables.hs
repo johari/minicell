e7d489482282da8a0e84682cc08258df426293de
Nima Johari <johari@ucdavis.edu>
2018-10-18 17:08:42 -0700

    Add example tables

---
+++ src/Examples/ExampleTables.hs
@@ -0,0 +1,15 @@
+module Minicell.Examples.ExampleTables where
+
+salesTableExample = [
+    [ "Sales Manager"   , "Product" , "Date"     , "Sales" , "", "Manager Name"]
+  , [ "Bill Wanton"     , "Books"   , "1-Aug-09" , "77"    , "", "Bill Wanton"]
+  , [ "Bill Wanton"     , "Pens"    , "2-Aug-09" , "80"    , "", ""]
+  , [ "Tim Spencer"     , "Books"   , "1-Aug-09" , "125"   , "", "Number of times appearing"]
+  , [ "Damon Hill"      , "Books"   , "2-Aug-09" , "250"   , "", "??"]
+  , [ "Damon Hill"      , "Books"   , "2-Aug-09" , "880"   , "", ""]
+  , [ "Bill Wanton"     , "Books"   , "1-Aug-09" , "925"   , "", ""]
+  , [ "Bill Wanton"     , "Books"   , "2-Aug-09" , "915"   , "", ""]
+  , [ "Terri Spencer"   , "Books"   , "1-Aug-09" , "400"   , "", ""]
+  , [ "Terri Spencer"   , "Pens"    , "2-Aug-09" , "420"   , "", ""]
+  ]
+

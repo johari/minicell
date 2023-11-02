4f243e6a40a38661c13bf0eb27c9f90c83410810
Nima Johari <johari@ucdavis.edu>
2023-11-01 19:56:09 -0700

    .

--- src/Piet/DSL/Poppet.hs
+++
@@ -1,7 +0,0 @@
-module Piet.DSL.Poppet where
-
-import Spreadsheet.Types
-
-eval' :: (Spreadsheet -> Formula -> IO Formula) -> Spreadsheet -> Formula -> IO Formula
-eval' eval model expr = case normalizeOp expr of
-  _ -> return $ ENotImplemented
